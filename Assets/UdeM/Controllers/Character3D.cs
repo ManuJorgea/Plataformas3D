23833d967fefc46f65f0f6b234adff03ac657138
ManuJorgea <69644681+ManuJorgea@users.noreply.github.com>
2024-04-04 19:40:08 -0500

    Movimiento de Personaje

--- Assets/UdeM/Controllers/Character3D.cs
+++ Assets/UdeM/Controllers/Character3D.cs
@@ -7,14 +7,19 @@ namespace UdeM.Controllers {
     public class Character3D : EntityController
     {
         protected CharacterController _cc;
-        protected float _gravity = -9.81f;
+        protected float _gravity = -0.981f;
         protected Vector3 _velocity;
         protected Vector3 _direction;
 
+        protected float _turnSmoothTime;
+        protected float _turnSmoothVelocity;
 
         protected override void Awake()
         {
             base.Awake();
+            _turnSmoothTime = 0.2f;
+            _turnSmoothVelocity = 0f;
+            _jumpForce = 0.03f;
             _cc = gameObject.GetComponent<CharacterController>();
 
             if (_cc == null )
@@ -24,22 +29,31 @@ namespace UdeM.Controllers {
 
         }
 
-
         protected override void Update()
         {
             base.Update();
             _isGrounded = _cc.isGrounded;
+        }
 
+        protected override void LateUpdate() {
+            base.LateUpdate();
 
             /* Velocity Y */
             _velocity.y += _gravity * Time.deltaTime;
 
             if (_isGrounded && _velocity.y < 0)
             {
-                _velocity.y = -1.81f;
+                _velocity.y = -0.181f;
             }
 
             _cc.Move(_velocity);
+        }
+
+        protected override void Jump()
+        {
+            base.Jump();
+
+            _velocity.y = Mathf.Sqrt(_jumpForce * -2 * _gravity);
         }
     }
 
--- Assets/UdeM/Controllers/Character3DPlayer.cs
+++ Assets/UdeM/Controllers/Character3DPlayer.cs
@@ -18,6 +18,23 @@ namespace UdeM.Controllers {
             _axisV = Input.GetAxis("Vertical");
 
             _direction = new Vector3(_axisH, 0, _axisV).normalized;
+
+            Debug.Log(_direction.magnitude);
+
+            if (_direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+
+                transform.rotation = Quaternion.Euler(0, angle, 0);
+                Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+                _cc.Move(moveDir.normalized * _speed * Time.deltaTime);
+            }
+
+            if (_isGrounded && Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
         }
     }
 }
